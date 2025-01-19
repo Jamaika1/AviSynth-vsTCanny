c3bb3831f0e5921ad7097b304e7f5beb88f4d118
Jamaika1 <lukaszcz18@wp.pl>
2025-01-19 21:40:58 +0100

    Defined _WIN32

--- src/vsTCanny.cpp
+++ src/vsTCanny.cpp
@@ -6,7 +6,7 @@
 AVS_FORCEINLINE void* aligned_malloc(size_t size, size_t align)
 {
     void* result = [&]() {
-#if defined(_MSC_VER) || defined(__GNUC__)
+#ifdef _WIN32 
         return _aligned_malloc(size, align);
 #else
         if (posix_memalign(&result, align, size))
@@ -21,7 +21,7 @@ AVS_FORCEINLINE void* aligned_malloc(size_t size, size_t align)
 
 AVS_FORCEINLINE void aligned_free(void* ptr)
 {
-#if defined(_MSC_VER) || defined(__GNUC__)
+#ifdef _WIN32 
     _aligned_free(ptr);
 #else
     free(ptr);
